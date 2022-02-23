6b73da5d730890e833cd555a48d758eab6c34805
filipe.olegini <filipe.olegini@totvs.com.br>
2022-02-23 15:34:30 -0300

    Primeiro Commit

---
+++ src/main/java/com/folegini/mcjava/McJavaApplication.java
@@ -0,0 +1,13 @@
+package com.folegini.mcjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folegini/mcjava/McJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folegini.mcjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
