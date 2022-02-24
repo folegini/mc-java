4bc528d99f50990ac37c84b30f17961b0de9fcf1
filipe.olegini <filipe.olegini@totvs.com.br>
2022-02-24 12:31:56 -0300

    Adicionado configurações de banco e ajustado a entedidade categoria

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,13 @@
 
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+4
+mvnw,5/d/5d837c4ed85ecaaf932c506e80ff5d7b9f3d590d
+8
+mvnw.cmd,1/f/1f1763f358c257ea3515417352b82b0165647ad1
+7
+HELP.md,1/a/1a273f73b7a477b0745593641bd708a6182c7ad5
+m
+=src/test/java/com/folegini/mcjava/McJavaApplicationTests.java,c/d/cd2b31326eb2c9294b6a7b71511f047a6aa87bfd
+h
+8src/main/java/com/folegini/mcjava/McJavaApplication.java,a/4/a4baffc3286dfdda47e819523e9c9a0f2101878c
--- src/main/java/com/folegini/mcjava/domain/Categoria.java
+++ src/main/java/com/folegini/mcjava/domain/Categoria.java
@@ -3,15 +3,23 @@ package com.folegini.mcjava.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = -4362280267044798130L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String nome;
